aabde6eea26f63e782418d8532d37b8e13a69eee
Kiattisak Anoochitarom <iMacbaszii@Kiattisaks-MacBook-Air.local>
2012-08-11 11:44:47 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookConnect::Application.config.secret_token = '010214db4fe96bfba075070d85ec58f54701defce3d014322597fc9fe56f4a9267d9d575ae37a9e75eb9f0db04e78169b3577f0cc07e1d208648c26dac076f02'
